6f56fe717307f98eca853f93e567fcc4b0290924
wnoel11 <43223925+wnoel11@users.noreply.github.com>
2018-11-12 21:14:51 -0500

    Add project files.

---
+++ Lab09/MyMath.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class MyMath
+    {
+  
+        public double result;
+        public double operand1;
+        public double operand2;
+
+        public void Multiply(double operand1, double operand2)
+        {
+            result = operand1 * operand2;
+        }
+
+        public void Divide(double operand1, double operand2)
+        {
+            result = operand1 / operand2;
+        }
+
+        public void Subtract(double operand1, double operand2)
+        {
+            result = operand1 - operand2; 
+        }
+
+        public void Add(double operand1, double operand2)
+        {
+            result = operand1 + operand2;
+        }
+
+        public double GetResult()
+        {
+            Console.WriteLine("The sum of the equation is");
+            return result;
+        }
+    }
+
+}
+    
+    
+
+    
+
+    
+
---
+++ Lab09/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath myResult1 = new MyMath();
+            myResult1.Multiply(2, 5);
+            Console.WriteLine(myResult1.GetResult());
+
+            myResult1.Divide(10, 5);
+            Console.WriteLine(myResult1.GetResult());
+
+            myResult1.Subtract(7, 6);
+            Console.WriteLine(myResult1.GetResult());
+
+            myResult1.Add(8, 4);
+            Console.WriteLine(myResult1.GetResult());
+        }
+
+    }
+}
